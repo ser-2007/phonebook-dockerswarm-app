b029cb5cb8b444ba34c03d41399c852c6848762c
Serkan <sersah95@gmail.com>
2021-10-02 18:24:24 +0300

    last changes

--- phonebook-app.py
+++ phonebook-app.py
@@ -98,7 +98,7 @@ def find_records():
     if request.method == 'POST':
         keyword = request.form['username']
         persons = find_persons(keyword)
-        return render_template('index.html', persons=persons, keyword=keyword, show_result=True, developer_name='Callahan')
+        return render_template('index.html', persons=persons, keyword=keyword, show_result=True, developer_name='Serkan')
     else:
         return render_template('index.html', show_result=False, developer_name='Serkan')
 
@@ -111,20 +111,20 @@ def add_record():
     if request.method == 'POST':
         name = request.form['username']
         if name is None or name.strip() == "":
-            return render_template('add-update.html', not_valid=True, message='Invalid input: Name can not be empty', show_result=False, action_name='save', developer_name='Callahan')
+            return render_template('add-update.html', not_valid=True, message='Invalid input: Name can not be empty', show_result=False, action_name='save', developer_name='Serkan')
         elif name.isdecimal():
-            return render_template('add-update.html', not_valid=True, message='Invalid input: Name of person should be text', show_result=False, action_name='save', developer_name='Callahan')
+            return render_template('add-update.html', not_valid=True, message='Invalid input: Name of person should be text', show_result=False, action_name='save', developer_name='Serkan')
 
         phone_number = request.form['phonenumber']
         if phone_number is None or phone_number.strip() == "":
-            return render_template('add-update.html', not_valid=True, message='Invalid input: Phone number can not be empty', show_result=False, action_name='save', developer_name='Callahan')
+            return render_template('add-update.html', not_valid=True, message='Invalid input: Phone number can not be empty', show_result=False, action_name='save', developer_name='Serkan')
         elif not phone_number.isdecimal():
-            return render_template('add-update.html', not_valid=True, message='Invalid input: Phone number should be in numeric format', show_result=False, action_name='save', developer_name='Callahan')
+            return render_template('add-update.html', not_valid=True, message='Invalid input: Phone number should be in numeric format', show_result=False, action_name='save', developer_name='Serkan')
 
         result = insert_person(name, phone_number)
-        return render_template('add-update.html', show_result=True, result=result, not_valid=False, action_name='save', developer_name='Callahan')
+        return render_template('add-update.html', show_result=True, result=result, not_valid=False, action_name='save', developer_name='Serkan')
     else:
-        return render_template('add-update.html', show_result=False, not_valid=False, action_name='save', developer_name='Callahan')
+        return render_template('add-update.html', show_result=False, not_valid=False, action_name='save', developer_name='Serkan')
 
 # Write a function named `update_record` which updates the record in the db using `GET` and `POST` methods,
 # using template files named `add-update.html` given under `templates` folder
